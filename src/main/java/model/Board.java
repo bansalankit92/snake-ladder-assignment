3aa981c58cb8b7431ad25483b85905f00392de57
Ankit Bansal <ankit@antbrains.com>
2020-10-17 10:31:57 +0530

    added models

---
+++ src/main/java/model/Board.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Board {
+    private int size;
+    private List<Snake> snakes;
+    private List<Ladder> ladders;
+    private Map<String, Integer> playerPieces;
+
+    public Board(int size) {
+        this.size = size;
+        this.snakes = new ArrayList<>();
+        this.ladders = new ArrayList<>();
+        this.playerPieces = new HashMap<>();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public List<Snake> getSnakes() {
+        return snakes;
+    }
+
+    public List<Ladder> getLadders() {
+        return ladders;
+    }
+
+    public Map<String, Integer> getPlayerPieces() {
+        return playerPieces;
+    }
+
+}
